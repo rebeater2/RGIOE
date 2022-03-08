304244825e29613f4f62ad33fe8184b0c6191934
rebeater <rebeater@qq.com>
2022-03-08 21:17:52 +0800

    [new feature] 小工具,修复IMU时间戳问题

--- Tools/ImuFix.cpp
+++ Tools/ImuFix.cpp
@@ -1,30 +1,24 @@
 /**
 * @file ImuFix.cpp in LooselyCouple2020_cpp
 * @author rebeater
-* @comment
+* @comment IMU时间戳修复
 * Create on 1/11/22 11:19 AM
 * @version 1.0
 **/
 #include "FileIO.h"
 int main(int argc, char **argv) {
-  std::string filename = "/media/rebeater/hd_data2/workspace/raw_data/2022/20220106/1/RAW_220107_455721.RAW.imutxt";
-  int rate = 200;
+  std::string filename = "/media/rebeater/hd_data2/workspace/raw_data/2022/20220307/ADIS16465_01/06/ADI51_220307_122202.raw.imd";
+  int rate = 125;
   double dt = 1.0 / rate;
-  IMUReader reader(filename, IMU_FILE_IMUTXT, IMU_FRAME_FRD, true, rate);
+  IMUReader reader(filename, IMU_FILE_IMD, IMU_FRAME_FRD, true, rate);
   ImuData imu;
   reader.ReadNext(imu);
   ImuData pre = imu;
-  ofstream out{filename + "fix.imutxt"};
+  ofstream out{filename + ".fix.imd",ios::binary};
   while (reader.IsOk()) {
 	reader.ReadNext(imu);
-	while (imu.gpst - pre.gpst > dt * 1.5) {
-	  /*用旧的数据填补缺失数据*/
-	  /*其实这么做不科学*/
-	  pre.gpst += dt;
-	  out << pre << '\n';
-	}
-	out << imu << '\n';
-	pre = imu;
+	imu.gpst -= 1.0;
+	out.write((const char *)&imu,sizeof imu);
   }
   return 0;
 }
