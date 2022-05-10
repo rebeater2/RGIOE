95c125b2385192e6eb5f38fd4e8422523361ae12
rebeater <rebeater@qq.com>
2022-05-10 21:20:23 +0800

    增加测试用例

--- Test/test_ins.cpp
+++ Test/test_ins.cpp
@@ -11,8 +11,8 @@ using namespace std;
 
 int main() {
     cout << "hello ins" << endl;
-    ifstream f_imu(R"(/media/rebeater/hd_data2/workspace/raw_data/demo/demo_machanization/Data1.bin)", ios::binary);
-    ofstream f_nav(R"(/media/rebeater/hd_data2/workspace/raw_data/demo/demo_machanization/lc2020.nav)", ios::out);
+    ifstream f_imu(R"(/mnt/d/NavData/demo/demo_machanization/Data1.bin)", ios::binary);
+    ofstream f_nav(R"(/mnt/d/NavData/demo/demo_machanization/rgioe.nav)", ios::out);
     ImuData imu;
     Vec3d pos = Vec3d{23.137395000003920 * _deg, 113.371364999992025 * _deg, 2.174999376267192};
     Vec3d vn = Vec3d{0.000174232435635, -0.000326994722535, 0.000249493122969};
@@ -31,6 +31,5 @@ int main() {
     cout<<ins.Output()<<endl;
     f_imu.close();
     f_nav.close();
-
-
+	return 0;
 }
