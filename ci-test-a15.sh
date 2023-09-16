789178e79a9aa3a0024fa8d0043257b480a659fd
rebeater <rebeater@qq.com>
2023-09-16 11:01:09 +0800

    [fix] update cicd

--- ci-test-a15.sh
+++ ci-test-a15.sh
@@ -1,2 +1,2 @@
-./App/bin/PostDataFusion.exe /home/rebeater/workspace/CICD/testdata/demo_a15.yml
+./App/bin/PostDataFusion /home/rebeater/workspace/CICD/testdata/demo_a15.yml
 cd Tools && python ErrorAnalyse.py "/home/rebeater/workspace/CICD/testdata/demo_a15.yml" 0.04 0.05
