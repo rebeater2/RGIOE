8b2f925c754652c36fc827233961d78a6391d38f
rebeater <rebeater@qq.com>
2023-09-16 10:54:07 +0800

    [fix] Update CICD configure

--- ci-test-a15.sh
+++ ci-test-a15.sh
@@ -1,2 +1,2 @@
-./App/bin/PostDataFusion.exe /home/linfe/workspace/CICD/testdata/demo_a15.yml
-cd Tools && python ErrorAnalyse.py "D:\Geek\Winlibs\msys2\home\linfe\workspace\CICD\testdata\demo_a15.yml" 0.04 0.05
+./App/bin/PostDataFusion.exe /home/rebeater/workspace/CICD/testdata/demo_a15.yml
+cd Tools && python ErrorAnalyse.py "/home/rebeater/workspace/CICD/testdata/demo_a15.yml" 0.04 0.05
