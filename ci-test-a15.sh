7a34a520b2838e8da693d3756c7708ae166ec6c7
rebeater <rebeater@qq.com>
2023-09-17 11:35:08 +0800

    [CFG] config cicd

--- ci-test-a15.sh
+++ ci-test-a15.sh
@@ -1,2 +1,4 @@
-./App/bin/PostDataFusion /home/rebeater/workspace/CICD/testdata/demo_a15.yml
-cd Tools && python ErrorAnalyse.py "/home/rebeater/workspace/CICD/testdata/demo_a15.yml" 0.04 0.05
+wget http://192.168.3.5:5081/dav/rebeater/CICD/testdata/demo_A15.tar.gz
+wget http://192.168.3.5:5081/dav/rebeater/CICD/testdata/demo_a15.yml
+./App/bin/PostDataFusion demo_a15.yml
+cd Tools && python ErrorAnalyse.py demo_a15.yml 0.04 0.05
