f9c9d0fe151218b490fd511965b2cece1576e3cf
rebeater <rebeater@qq.com>
2023-09-24 17:49:42 +0800

    [CICD] add ci test for ins

--- ci-test-pos830.sh
+++ ci-test-pos830.sh
@@ -2,4 +2,4 @@ wget http://192.168.3.5:5081/dav/rebeater/CICD/testdata/demo_pos830.tar.gz
 wget http://192.168.3.5:5081/dav/rebeater/CICD/testdata/demo_pos830.yml
 tar -xvf demo_pos830.tar.gz
 ./App/bin/PostDataFusion demo_pos830.yml
-python Tools/ErrorAnalyse.py demo_pos830.yml 0.04 0.05
+python Tools/ErrorAnalyse.py demo_pos830.yml 0.01 0.05
